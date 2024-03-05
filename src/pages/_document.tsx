2cd1e1f1a238580c064b8e04f3ad74663a2d2dba
Julio Freitas <julio.martins.freitas@gmail.com>
2024-03-04 21:30:09 -0300

    add tracker

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -65,14 +65,16 @@ export default function MyDocument({ emotionStyleTags }: MyDocumentProps) {
         ></script>
 
         {/* rastreamento do analytivs. favor manter */}
-        <script>
-            (function(w,d,t,u,n,a,m){w['MauticTrackingObject']=n;
-                w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)},a=d.createElement(t),
-                m=d.getElementsByTagName(t)[0];a.async=1;a.src=u;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','https://mautic.lion.cherryit.co/mtc.js','mt');
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+              (function(w,d,t,u,n,a,m){w['MauticTrackingObject']=n;
+                  w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)},a=d.createElement(t),
+                  m=d.getElementsByTagName(t)[0];a.async=1;a.src=u;m.parentNode.insertBefore(a,m)
+              })(window,document,'script','https://mautic.lion.cherryit.co/mtc.js','mt');
 
-            mt('send', 'pageview');
-        </script>
+              mt('send', 'pageview');
+          `}
+        </Script>
 
         {/*
         <script
