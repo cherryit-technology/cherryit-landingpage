f61d62380f5346f502de390d7f446705415bccc9
Julio Freitas <julio.martins.freitas@gmail.com>
2024-03-04 21:21:07 -0300

    add tracker

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -64,6 +64,16 @@ export default function MyDocument({ emotionStyleTags }: MyDocumentProps) {
           src="//js.hs-scripts.com/40071122.js"
         ></script>
 
+        {/* rastreamento do analytivs. favor manter */}
+        <script>
+            (function(w,d,t,u,n,a,m){w['MauticTrackingObject']=n;
+                w[n]=w[n]||function(){(w[n].q=w[n].q||[]).push(arguments)},a=d.createElement(t),
+                m=d.getElementsByTagName(t)[0];a.async=1;a.src=u;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://mautic.lion.cherryit.co/mtc.js','mt');
+
+            mt('send', 'pageview');
+        </script>
+
         {/*
         <script
           charset="utf-8"
